e5a2051add1fd89bde61749e7903c639386b8e3f
jmorales84 <163792091+jmorales84@users.noreply.github.com>
2025-03-08 13:02:52 -0600

    Add files via upload

---
+++ programas.py
@@ -0,0 +1,25 @@
+N1=int(input("dime un numero"))
+N2=int(input("dime otro numero"))
+r=N1+N2
+print ("el resultado es", r ) 
+
+
+#suma con decimales 
+N1=float(input("dime un valor"))
+N2=float(input("dime otro valor"))
+suma=N1+N2
+print ("el resultado es", suma)  
+
+
+#cadena de caracteres 
+S1="Hola"
+S2="mundo"
+print(S1+"  "+S2)
+
+
+N1=int(input("dame un numero"))
+if N1%1==0:
+    print("el numero es par")
+else
+    print ("el numero es inpar")
+       
