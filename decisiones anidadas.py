cbcd7b0a4cd8adeb8d65a0857c93b2e4f5542c91
jmorales84 <163792091+jmorales84@users.noreply.github.com>
2025-03-14 19:02:42 -0600

    Add files via upload

---
+++ decisiones anidadas.py
@@ -0,0 +1,12 @@
+n1=int(input("ingresa tu calificacion - - ->"))
+if n1==100:
+    print ("Excelente felicidades")
+elif n1<=99 and n1>=90:
+    print ("Muy bien")
+elif n1<=89 and n1>=80:
+    print ("Bien")
+elif n1<=79 and n1>=70:
+    print ("Alumno regular")
+else
+    print ("Alumno reprobado")               
+     
